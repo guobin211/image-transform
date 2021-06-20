51fc88a142516cf27bbea7bef19926dda215c8e7
michaelbguo <michaelbguo@tencent.com>
2021-06-20 12:04:53 +0800

    feat(http): request

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,36 @@ use crate::generic_type::run_generic_type;
 use crate::io_path::run_file_io;
 #[allow(unused_imports)]
 use crate::link_list::run_link_list;
+use std::collections::HashMap;
 
-fn main() {
-    // run_basic_mod();
-    // run_link_list();
-    run_file_io();
-    // run_generic_type();
+#[allow(dead_code)]
+async fn get() {
+    let resp = reqwest::get("https://httpbin.org/ip");
+    match resp.await {
+        Ok(resp) => {
+            println!("{:#?}", resp);
+            let text = resp.json::<HashMap<String, String>>();
+            match text.await {
+                Ok(json) => {
+                    println!("{:#?}", json);
+                }
+                Err(err) => {
+                    println!("{:#?}", err);
+                }
+            }
+        }
+        Err(err) => {
+            println!("{:#?}", err);
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
